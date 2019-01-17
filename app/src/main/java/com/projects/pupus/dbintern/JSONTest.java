ed3ee0a1c387d1561f94e919c290e1a138728569
rurfy <chrissy.richter@web.de>
2019-01-17 15:44:11 +0100

    - erste Verknüpfung JSON mit GUI - Ungetestet! - list_item.xml erstellt zur Darstellung eines Items der Liste

--- app/src/main/java/com/projects/pupus/dbintern/JSONTest.java
+++ app/src/main/java/com/projects/pupus/dbintern/JSONTest.java
@@ -26,9 +26,9 @@ public class JSONTest extends AppCompatActivity{
         @Override
         protected void onCreate(Bundle savedInstanceState) {
             super.onCreate(savedInstanceState);
-            setContentView(R.layout.activity_main);
+            setContentView(R.layout.sperrliste);
 
-            contactList = new ArrayList<>();
+            ortList = new ArrayList<>();
             lv = (ListView) findViewById(R.id.list);
 
             new GetContacts().execute();
