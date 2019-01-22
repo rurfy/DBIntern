20e113de324e1f41bdb1f2dea4e30c8f11ea5214
Lucmie98 <lucmie98@gmail.com>
2019-01-22 22:29:45 +0100

    Alle XMLs und Java Classes erstellt

--- app/src/main/java/com/projects/pupus/dbintern/Ueber.java
+++ app/src/main/java/com/projects/pupus/dbintern/Ueber.java
@@ -1,5 +1,6 @@
 package com.projects.pupus.dbintern;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
@@ -18,6 +19,25 @@ public class Ueber extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 finish();
+            }
+        });
+
+        Button anmerkung = (Button) findViewById(R.id.anmerkungbutton);
+        Button version = (Button) findViewById(R.id.versionenbutton);
+
+        anmerkung.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent (Ueber.this, Anmerkung.class);
+                startActivity(intent);
+            }
+        });
+
+        version.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Ueber.this, Changelog.class);
+                startActivity(intent);
             }
         });
     }
