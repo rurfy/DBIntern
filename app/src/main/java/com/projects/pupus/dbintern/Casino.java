9795829a7d8a6612e3591e4a0fa5b8ce243c71ea
rurfy <chrissy.richter@web.de>
2019-01-23 21:04:49 +0100

    - Tests für JSON abgeschlossen - Keine Weiteren Adaptionen

--- app/src/main/java/com/projects/pupus/dbintern/Casino.java
+++ app/src/main/java/com/projects/pupus/dbintern/Casino.java
@@ -4,14 +4,25 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ListAdapter;
+import android.widget.ListView;
+import android.widget.SimpleAdapter;
 import android.widget.TextView;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+
 public class Casino extends AppCompatActivity {
+
+    private String TAG = Casino.class.getSimpleName();
+    private ListView lv;
+
+    ArrayList<HashMap<String, String>> list;
 
     @Override
     protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.casrab);
+        setContentView(R.layout.sperrliste);
 
         Bundle b = getIntent().getExtras();
         String name = b.getString("titel");
@@ -21,11 +32,13 @@ public class Casino extends AppCompatActivity {
 
         titel.setText(name);
 
+
         zurueck.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 finish();
             }
         });
+
     }
 }
--- app/src/main/java/com/projects/pupus/dbintern/JSONTest.java
+++ app/src/main/java/com/projects/pupus/dbintern/JSONTest.java
@@ -4,6 +4,8 @@ import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 import android.widget.ListAdapter;
 import android.widget.ListView;
 import android.widget.SimpleAdapter;
@@ -18,100 +20,109 @@ import java.util.HashMap;
 
 public class JSONTest extends AppCompatActivity{
 
-        private String TAG = JSONTest.class.getSimpleName();
-        private ListView lv;
+    private String TAG = JSONTest.class.getSimpleName();
+    private ListView lv;
 
-        ArrayList<HashMap<String, String>> ortList;
+    ArrayList<HashMap<String, String>> ortList;
 
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.sperrliste);
+
+        ortList = new ArrayList<>();
+        lv = (ListView) findViewById(R.id.list);
+
+        Button zurueck = (Button) findViewById(R.id.zurueck);
+
+        zurueck.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+
+        new GetContacts().execute();
+    }
+
+    private class GetContacts extends AsyncTask<Void, Void, Void> {
         @Override
-        protected void onCreate(Bundle savedInstanceState) {
-            super.onCreate(savedInstanceState);
-            setContentView(R.layout.sperrliste);
+        protected void onPreExecute() {
+            super.onPreExecute();
+            Toast.makeText(JSONTest.this,"Json Data is downloading",Toast.LENGTH_LONG).show();
 
-            ortList = new ArrayList<>();
-            lv = (ListView) findViewById(R.id.list);
-
-            new GetContacts().execute();
         }
 
-        private class GetContacts extends AsyncTask<Void, Void, Void> {
-            @Override
-            protected void onPreExecute() {
-                super.onPreExecute();
-                Toast.makeText(JSONTest.this,"Json Data is downloading",Toast.LENGTH_LONG).show();
+        @Override
+        protected Void doInBackground(Void... arg0) {
+            HttpHandler sh = new HttpHandler();
+            // Making a request to url and getting response
+            String url = "http://dbintern.appshost.net/api.php?pass=db&db=casino_ort";
+            String jsonStr = sh.makeServiceCall(url);
 
-            }
+            Log.e(TAG, "Response from url: " + jsonStr);
+            //Log.e(TAG, jsonStr);
+            if (jsonStr != null) {
+                try {
+                    //JSONObject jsonObj = new JSONObject(jsonStr);
 
-            @Override
-            protected Void doInBackground(Void... arg0) {
-                HttpHandler sh = new HttpHandler();
-                // Making a request to url and getting response
-                String url = "http://dbintern.appshost.net/api.php?pass=db&db=casino_ort";
-                String jsonStr = sh.makeServiceCall(url);
+                    // JSONArray is created
+                    JSONArray jsonOrte = new JSONArray(jsonStr);
+                    //JSONArray jsonOrte = jsonObj.getJSONArray("casino_orte");
 
-                Log.e(TAG, "Response from url: " + jsonStr);
-                //Log.e(TAG, jsonStr);
-                if (jsonStr != null) {
-                    try {
-                        //JSONObject jsonObj = new JSONObject(jsonStr);
+                    // looping through all values
+                    for (int i = 0; i < jsonOrte.length(); i++) {
+                        JSONObject c = jsonOrte.getJSONObject(i);
+                        String id = c.getString("Ort");
+                        String name = c.getString("Ort_ID");
+                        Log.e(TAG, id + name);
 
-                        // JSONArray is created
-                        JSONArray jsonOrte = new JSONArray(jsonStr);
-                        //JSONArray jsonOrte = jsonObj.getJSONArray("casino_orte");
+                        // tmp hash map for single object
+                        HashMap<String, String> ort = new HashMap<>();
 
-                        // looping through all values
-                        for (int i = 0; i < jsonOrte.length(); i++) {
-                            JSONObject c = jsonOrte.getJSONObject(i);
-                            String id = c.getString("Ort");
-                            String name = c.getString("Ort_ID");
-                            Log.e(TAG, id + name);
+                        // adding each child node to HashMap key => value
+                        ort.put("Ort", id);
+                        ort.put("Ort_ID", name);
 
-                            // tmp hash map for single object
-                            HashMap<String, String> ort = new HashMap<>();
-
-                            // adding each child node to HashMap key => value
-                            ort.put("Ort", id);
-                            ort.put("Ort_ID", name);
-
-                            // adding object to arrayList
-                            ortList.add(ort);
-                        }
-                    } catch (final JSONException e) {
-                        Log.e(TAG, "Json parsing error: " + e.getMessage());
-                        runOnUiThread(new Runnable() {
-                            @Override
-                            public void run() {
-                                Toast.makeText(getApplicationContext(),
-                                        "Json parsing error: " + e.getMessage(),
-                                        Toast.LENGTH_LONG).show();
-                            }
-                        });
-
+                        // adding object to arrayList
+                        ortList.add(ort);
                     }
-
-                } else {
-                    Log.e(TAG, "Couldn't get json from server.");
+                } catch (final JSONException e) {
+                    Log.e(TAG, "Json parsing error: " + e.getMessage());
                     runOnUiThread(new Runnable() {
                         @Override
                         public void run() {
                             Toast.makeText(getApplicationContext(),
-                                    "Couldn't get json from server. Check LogCat for possible errors!",
+                                    "Json parsing error: " + e.getMessage(),
                                     Toast.LENGTH_LONG).show();
                         }
                     });
+
                 }
 
-                return null;
+            } else {
+                Log.e(TAG, "Couldn't get json from server.");
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        Toast.makeText(getApplicationContext(),
+                                "Couldn't get json from server. Check LogCat for possible errors!",
+                                Toast.LENGTH_LONG).show();
+                    }
+                });
             }
 
-            @Override
-            protected void onPostExecute(Void result) {
-                super.onPostExecute(result);
-                ListAdapter adapter = new SimpleAdapter(JSONTest.this, ortList,
-                        R.layout.list_item, new String[]{ "Ort","Ort_ID"},
-                        new int[]{R.id.email, R.id.mobile});
-                lv.setAdapter(adapter);
-            }
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(Void result) {
+            super.onPostExecute(result);
+            ListAdapter adapter = new SimpleAdapter(JSONTest.this, ortList,
+                    R.layout.list_item, new String[]{ "Ort","Ort_ID"},
+                    new int[]{R.id.email, R.id.mobile});
+            lv.setAdapter(adapter);
         }
     }
+}
 
