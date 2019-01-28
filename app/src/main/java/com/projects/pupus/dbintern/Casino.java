8353b00ab352cf79d402ca4536bc9ee62704fb79
Lucmie98 <lucmie98@gmail.com>
2019-01-28 21:42:07 +0100

    Datenbank zugriff in Casrab

--- app/src/main/java/com/projects/pupus/dbintern/Casino.java
+++ app/src/main/java/com/projects/pupus/dbintern/Casino.java
@@ -3,6 +3,7 @@ package com.projects.pupus.dbintern;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.Button;
 import android.widget.ListAdapter;
 import android.widget.ListView;
@@ -15,14 +16,17 @@ import java.util.HashMap;
 public class Casino extends AppCompatActivity {
 
     private String TAG = Casino.class.getSimpleName();
-    private ListView lv;
+    private ListView lvCasino;
 
-    ArrayList<HashMap<String, String>> list;
+    ArrayList<HashMap<String, String>> ortList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.casrab);
+
+        ortList = new ArrayList<>();
+        lvCasino = (ListView) findViewById(R.id.lvCasrab);
 
         Bundle b = getIntent().getExtras();
         String name = b.getString("titel");
@@ -39,6 +43,31 @@ public class Casino extends AppCompatActivity {
                 finish();
             }
         });
+
+        lvCasino.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapter, View view, int position, long id) {
+
+                finish();
+            }
+        });
+
+        JSONParser getContacts = new com.projects.pupus.dbintern.JSONParser(Casino.this, TAG, ortList, new String[] {"Ort", "Ort_ID"}, new String[] {"Ort"},  "http://dbintern.appshost.net/api.php?pass=db&db=casino_ort") {
+            @Override
+            protected void onPostExecute(Void result) {
+                super.onPostExecute(result);
+                ListAdapter adapter = new SimpleAdapter(Casino.this, ortList,
+                        R.layout.casrab_list_item, new String[]{"Ort"},
+                        new int[]{R.id.tvOrt});
+                lvCasino.setAdapter(adapter);
+            }
+
+            @Override
+            protected void putAll(HashMap<String, String> map, String[] listTags, String[] content) {
+                map.put(listTags[0], content[0]);
+            }
+        };
+        getContacts.execute();
 
     }
 }
